7d5ba34bbcf54115193244480e86c35da63d9101
gitp18 <developer.ipsita@gmail.com>
2018-08-06 12:22:28 +0530

    Static site,devided into componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SocialComponent } from './social/social.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ServicesComponent } from './services/services.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AboutComponent } from './about/about.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialComponent,
+    PricingComponent,
+    ClientsComponent,
+    ServicesComponent,
+    GalleryComponent,
+    AboutComponent,
+    TestimonialsComponent,
+    NavigationComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
